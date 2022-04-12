2adc7c46afc52a65eb3fb4bbc26b1aa680c8d33e
Erick Escalante Olano <ericks.escalanteolano@gmail.com>
2022-04-12 00:35:05 -0500

    asdasd

---
+++ las-gardenias/precache-manifest.f0189c550a4bca37fc3697954f3ea15b.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd18d44131b1cda2acd29629dfd4549c",
+    "url": "/las-gardenias/index.html"
+  },
+  {
+    "revision": "0e4742ef1dfb04e79b34",
+    "url": "/las-gardenias/static/css/2.6309c6bf.chunk.css"
+  },
+  {
+    "revision": "1fa03bd403a4c9aab5a6",
+    "url": "/las-gardenias/static/css/main.aca9f5b6.chunk.css"
+  },
+  {
+    "revision": "0e4742ef1dfb04e79b34",
+    "url": "/las-gardenias/static/js/2.4d56217e.chunk.js"
+  },
+  {
+    "revision": "0e6cb1fbd5179720173d2f6b0067c84b",
+    "url": "/las-gardenias/static/js/2.4d56217e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fa03bd403a4c9aab5a6",
+    "url": "/las-gardenias/static/js/main.bff8f8fc.chunk.js"
+  },
+  {
+    "revision": "9aae31a7e5d38f090f8b",
+    "url": "/las-gardenias/static/js/runtime-main.083847bb.js"
+  },
+  {
+    "revision": "476d5bd9a812489d65603967c5159109",
+    "url": "/las-gardenias/static/media/Etapa_1.476d5bd9.svg"
+  },
+  {
+    "revision": "1b3788592dd36e4cf92a74608dbbdb1e",
+    "url": "/las-gardenias/static/media/LogoGardenias.1b378859.png"
+  },
+  {
+    "revision": "a0e9769a424eb3d8982cbce193967b63",
+    "url": "/las-gardenias/static/media/LogoModal.a0e9769a.png"
+  },
+  {
+    "revision": "7f0f9aa160b8308917eb57b24ed51d49",
+    "url": "/las-gardenias/static/media/Lote.7f0f9aa1.jpg"
+  },
+  {
+    "revision": "fa2e494c12860f5d0e9f0b4429150031",
+    "url": "/las-gardenias/static/media/Modal.fa2e494c.jpg"
+  },
+  {
+    "revision": "d47f2380dd885e9b16abf6adb4019343",
+    "url": "/las-gardenias/static/media/Polygon.d47f2380.svg"
+  },
+  {
+    "revision": "5fe73054f391f1baf824dcc13a295d41",
+    "url": "/las-gardenias/static/media/area_comercial_icono.5fe73054.svg"
+  },
+  {
+    "revision": "f1cc26dc5ede7a2738f48c5677e11069",
+    "url": "/las-gardenias/static/media/area_deportiva_icono.f1cc26dc.svg"
+  },
+  {
+    "revision": "0fe32031ce39654d549d79d76a42cc9b",
+    "url": "/las-gardenias/static/media/area_reservada_icono.0fe32031.svg"
+  },
+  {
+    "revision": "146525426880d544c66dc46749c9bd87",
+    "url": "/las-gardenias/static/media/area_verde_icono.14652542.svg"
+  },
+  {
+    "revision": "5a26c47366c9095995e1bf420c3ced47",
+    "url": "/las-gardenias/static/media/avenida_icono.5a26c473.svg"
+  },
+  {
+    "revision": "f9966217c758914c5c7412a8e868b12a",
+    "url": "/las-gardenias/static/media/el_wheelchair.f9966217.svg"
+  },
+  {
+    "revision": "6432d0b51233ce158be12c4085be0857",
+    "url": "/las-gardenias/static/media/fontisto_close.6432d0b5.svg"
+  },
+  {
+    "revision": "c50a6923643279ee3fff6bfc361d8d4c",
+    "url": "/las-gardenias/static/media/iconhead.c50a6923.svg"
+  },
+  {
+    "revision": "147a4317fc00cd68b67af1dce4a79846",
+    "url": "/las-gardenias/static/media/lineas.147a4317.svg"
+  },
+  {
+    "revision": "425bbc099f0cc07c76fb5708d7166c89",
+    "url": "/las-gardenias/static/media/preferecial_icono.425bbc09.svg"
+  },
+  {
+    "revision": "ebc26e9981d519fb53257cc192016d1e",
+    "url": "/las-gardenias/static/media/rectangulo.ebc26e99.png"
+  },
+  {
+    "revision": "4d210df96225ec402c0d7883582fd26c",
+    "url": "/las-gardenias/static/media/road.4d210df9.svg"
+  }
+]);
